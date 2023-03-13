f34682785eab916306ff2a7cf28c4446e2fe7f2c
Nguyen_Huu_Thieng <16521532@gm.uit.edu.vn>
2023-03-13 16:40:35 +0700

    #9 - Axios-interceptor-and-Toastify

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,6 @@
-import axios, { type AxiosInstance } from 'axios'
+import axios, { type AxiosInstance, AxiosError } from 'axios'
+import { toast } from 'react-toastify'
+import HttpStatusCode from 'src/constants/httpStatusCode.enum'
 
 class Http {
   instance: AxiosInstance
@@ -10,6 +12,23 @@ class Http {
         'Content-Type': 'application/json'
       }
     })
+    // * interceptors middleware
+    this.instance.interceptors.response.use(
+      function (response) {
+        return response
+      },
+      function (error: AxiosError) {
+        // Bất kì mã trạng thái nào lọt ra ngoài tầm 2xx đều khiến hàm này được trigger\
+        // Làm gì đó với lỗi response
+        if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const data: any | undefined = error.response?.data
+          const message = data.message || error.message
+          toast.error(message)
+        }
+        return Promise.reject(error)
+      }
+    )
   }
 }
 
